ebd7544e3f528db53026602156f4d00f6f3b843e
Hola987 <sunnyD1119@gmail.com>
2017-04-01 14:46:55 -0700

    Create QuizQuestions.java

---
+++ QuizQuestions.java
@@ -0,0 +1,188 @@
+public class Questions {
+    public static void main(String[] args) {
+        questions.add("1) The term gigabyte refers to"); // 1
+
+        a.add("1024 bytes"); // 1
+        b.add("1024 kilobytes"); // 1
+        c.add("1024 megabytes"); // 1
+        d.add("1024 gigabytes"); // 1
+        e.add("None of the above"); // 1
+        correct.add("C"); // 1
+
+        questions.add("2) What is meant by the term RAM?"); // 2
+        a.add("Memory which can only be read"); // 2
+        b.add("Memory which can be both^read and written to"); // 2
+        c.add("Memory which is used for permanent storage"); // 2
+        d.add("Memory which can only be written to"); // 2
+        e.add("None of the above"); // 2
+        correct.add("B"); // 2
+
+
+        questions.add("3) A computer assisted method for the recording and analyzing of existing or hypothetical systems is");  // 3
+        a.add("Data transmission"); //3
+        b.add("Data flow"); //3
+        c.add("Data capture"); //3
+        d.add("Data processing"); //3
+        e.add("None of the above"); //3
+        correct.add("B"); //3
+
+
+        questions.add("The brain of any computer system is"); // 4
+        a.add("ALU"); // 4
+        b.add("Memory"); // 4
+        c.add("CPU"); // 4
+        d.add("Control unit"); // 4
+        e.add("None of the above"); // 4
+        correct.add("C"); // 4
+
+
+
+        questions.add("A programmer needs to produce a random real value x in the range 20.0 ≤ x < 30.0." +
+                " A statement that does this correctly is\n"); // 5
+        a.add("double x = Math.random( 20, 30);"); // 5
+        b.add("double x = Math.random(30) - 10;"); // 5
+        c.add("double x = Math.random(20) +10;"); // 5
+        d.add("double x = 10*Math.random( ) + 20;"); // 5
+        e.add("double x = 20*Math.random  ( ) = 10;"); // 5
+        correct.add("D"); // 5
+
+
+
+        questions.add("Which term is not used to describe the memory component of a computer?"); // 6
+        a.add("ROM"); // 6
+        b.add("SAM"); // 6
+        c.add("RAM"); // 6
+        d.add("PROM"); // 6
+        e.add("None of the above"); // 6
+        correct.add("B"); // 6
+
+
+        questions.add("Which of the following is the 1's complement of 10?"); // 7
+        a.add("01"); // 7
+        b.add("001"); // 7
+        c.add("110 "); // 7
+        d.add("100"); // 7
+        e.add("11"); // 7
+        correct.add("A"); // 7
+
+
+        questions.add("Magnetic tape can serve as :"); // 8
+        a.add("Secondary-storage media"); // 8
+        b.add("output media"); // 8
+        c.add("input media"); // 8
+        d.add("All of the above "); // 8
+        e.add("None of the above "); // 8
+        correct.add("D"); //8
+
+        questions.add("The binary system uses powers of"); // 9
+        a.add("16"); // 9
+        b.add("8"); // 9
+        c.add("2"); // 9
+        d.add("10"); // 9
+        e.add("None of the above"); // 9
+        correct.add("C"); // 9
+
+
+        questions.add("A computer program that converts assembly language to machine language is"); // 10
+        a.add("Compiler"); // 10
+        b.add("Interpreter"); // 10
+        c.add("Comparator"); // 10
+        d.add("Assembler"); // 10
+        e.add("None of the above"); // 10
+        correct.add("D"); // 10
+
+
+        questions.add( "11) \n"+
+                "4 5 6 2 1\n"+
+                "\n"+
+                "Which of the following statements will cause an error?");//11
+
+        a.add(" list.add(5); "); // 11
+        b.add("list.add( 2, 4); "); // 11
+        c.add("Integer int0b = list.get(5); "); // 11
+        d.add("Object ob = list.add( 5 ,7); "); // 11
+        e.add(" Integer x = list.set( 3, 0); "); // 11
+        correct.add("C"); //11
+
+
+
+
+
+        questions.add (" 12)Binary numbers need more places for counting because");//12
+
+        a.add("They are always big numbers"); // 12
+        b.add(" Any no. of 0's can be added in front of them "); // 12
+        c.add("Binary base is small "); // 12
+        d.add("0's and l's have to be properly spaced apart  "); // 12
+        e.add("None of the above"); // 12
+        correct.add("C"); //12
+
+        questions.add ("13)Which access method is used for obtaining a record from a cassette tape?");
+        a.add("Direct"); //13
+        b.add("Sequential "); //13
+        c.add(" Random "); //13
+        d.add("All of the above "); //13
+        e.add("None of the above"); //13
+        correct.add("B"); // 13
+
+        questions.add("14)The elements of array arr are to be sorted in increasing order. Which represents the first step of a mergesort algorithm? "); // 14
+        a.add(" Fine the smallest in arr and swap it into arr[0] "); //14
+        b.add(" compare arr [0] with arr[1] ");//14
+        c.add("compare arr[0] with te middle element of arr, arr[mid] "); //14
+        d.add(" compare the first and last element "); //14
+        e.add(" Break the array into roughly two equal halves "); //14
+        correct.add("E"); //14
+
+
+        questions.add( "15) The 2's complement of a binary no. is obtained by adding.....to its 1's complement.")
+
+        a.add("0"); // 15
+        b.add("1"); // 15
+        c.add("10 "); // 15
+        d.add("12"); // 15
+        e.add("14"); // 15
+        correct.add("B"); //15
+
+        questions.add("16)Any storage device added to a computer beyond the immediately usable main storage is known as");//16
+
+        a.add("Pizza Stick "); //16
+        b.add("Punched card "); //16
+        c.add("Hard disk "); //16
+        d.add("Floppy disk"); //16
+        e.add("Backing store"); //16
+        correct.add("E"); //16
+
+
+        questions.add("17)Which output device is used for translating information from a computer into pictorial form on paper.");
+        a.add(" Plotter "); //17
+        b.add("Mouse "); //17
+        c.add("Touch panel "); //17
+        d.add("Card punch"); //17
+        e.add("none of the above"); //17
+        correct.add("A");//17
+
+        questions.add("Which is a unit representing the no bits of discrete."); // 18
+        a.add("Byte"); // 18
+        b.add("Bit"); // 18
+        c.add("Baud"); // 18
+        d.add("All of the above"); // 18
+        e.add("None of the above"); // 18
+        correct.add("C"); // 18
+
+
+        questions.add("What is the name given to the 8-bit unit used for coding data?"); // 19
+        a.add(" Block of characters"); // 19
+        b.add("Byte"); // 19
+        c.add("Word"); // 19
+        d.add("K"); // 19
+        e.add("None of the above"); // 19
+        correct.add("B"); // 19
+
+
+        questions.add("Which is a false statement about static methods?"); // 20
+        a.add("A static method can use a static variable in its implementation code"); // 20
+        b.add("If a class is such that it doesn't make sense to ever create an object of the class"); // 20
+        c.add("All methods in a driver class that has a man method should be static"); // 20
+        d.add("A static method cannot be invoked by an object of a class"); // 20
+        e.add("Accessor methods in an object class may be static, depending on the value that’s being accessed"); // 20
+        correct.add("E"); // 20 
